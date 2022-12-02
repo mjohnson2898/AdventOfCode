a49d1567cc3420835481c287e782bcd45b63884b
mrj9235 <mrj9235@g.rit.edu>
2022-12-01 23:32:18 -0600

    Yes this code is bad, but yes, it works and got the problems right :)

--- main/src/part1.java
+++ main/src/part1.java
@@ -2,36 +2,70 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class part1 {
+public class Part1 {
+
+    private static final int rock = 1;
+    private static final int paper = 2;
+    private static final int scissors = 3;
+    private static final int win = 6;
+    private static final int loss = 0;
+    private static final int draw = 3;
+
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader;
-
-        try {
-            reader = new BufferedReader(new FileReader("main/resources/input.txt"));
-            String line = reader.readLine();
-
-            int currSum = 0;
-            int maxSum = 0;
-
-            while(line != null){
-                if(line.equals("")){
-                    if(currSum > maxSum){
-                        maxSum = currSum;
+            BufferedReader reader;
+            try {
+                reader = new BufferedReader(new FileReader("main/resources/input.txt"));
+                String line = reader.readLine();
+                int finalScore = 0;
+                while(line != null) {
+                    String[] round = line.split(" ");
+                    int roundScore = 0;
+                    String opponent = round[0];
+                    String me = round[1];
+                    if(opponent.equals("A")){
+                        if(me.equals("Y")){
+                            roundScore = paper + win;
+                        }
+                        else if(me.equals("X")){
+                            roundScore = rock + draw;
+                        }
+                        else{
+                            roundScore = scissors + loss;
+                        }
                     }
-                    currSum = 0;
+                    else if(opponent.equals("B")){
+                        if(me.equals("Z")){
+                            roundScore = scissors + win;
+                        }
+                        else if(me.equals("Y")){
+                            roundScore = paper + draw;
+                        }
+                        else{
+                            roundScore = rock + loss;
+                        }
+                    }
+                    else{
+                        if(me.equals("X")){
+                            roundScore = rock + win;
+                        }
+                        else if(me.equals("Z")){
+                            roundScore = scissors + draw;
+                        }
+                        else{
+                            roundScore = paper + loss;
+                        }
+                    }
+
+                    finalScore += roundScore;
+                    line = reader.readLine();
                 }
-                else{
-                    currSum += Integer.parseInt(line.strip());
-                }
-                line = reader.readLine();
+
+                System.out.println(finalScore);
+
             }
-
-            System.out.println(maxSum);
-
-        } catch (Exception e){
-            throw e;
-        }
-
-
+            catch (Exception e){
+                throw e;
+            }
     }
 }
--- main/src/part2.java
+++ main/src/part2.java
@@ -1,42 +1,70 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 
-public class part2 {
+public class Part2 {
+    private static final int rock = 1;
+    private static final int paper = 2;
+    private static final int scissors = 3;
+    private static final int win = 6;
+    private static final int loss = 0;
+    private static final int draw = 3;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader;
-
         try {
             reader = new BufferedReader(new FileReader("main/resources/input.txt"));
             String line = reader.readLine();
-
-            int currSum = 0;
-            ArrayList<Integer> allSums = new ArrayList<>();
-
-            while(line != null){
-                if(line.equals("")){
-                    allSums.add(currSum);
-                    currSum = 0;
+            int finalScore = 0;
+            while(line != null) {
+                String[] round = line.split(" ");
+                int roundScore = 0;
+                String opponent = round[0];
+                String me = round[1];
+                if(opponent.equals("A")){
+                    if(me.equals("Y")){
+                        roundScore = rock + draw;
+                    }
+                    else if(me.equals("X")){
+                        roundScore = scissors + loss;
+                    }
+                    else{
+                        roundScore = paper + win;
+                    }
+                }
+                else if(opponent.equals("B")){
+                    if(me.equals("Y")){
+                        roundScore = paper + draw;
+                    }
+                    else if(me.equals("X")){
+                        roundScore = rock + loss;
+                    }
+                    else{
+                        roundScore = scissors + win;
+                    }
                 }
                 else{
-                    currSum += Integer.parseInt(line.strip());
+                    if(me.equals("Y")){
+                        roundScore = scissors + draw;
+                    }
+                    else if(me.equals("X")){
+                        roundScore = paper + loss;
+                    }
+                    else{
+                        roundScore = rock + win;
+                    }
                 }
+
+                finalScore += roundScore;
                 line = reader.readLine();
             }
 
-            Collections.sort(allSums);
-            Collections.reverse(allSums);
+            System.out.println(finalScore);
 
-            int result = allSums.get(0) + allSums.get(1) + allSums.get(2);
-
-            System.out.println(result);
-
-        } catch (Exception e){
+        }
+        catch (Exception e){
             throw e;
         }
-
-
     }
 }
