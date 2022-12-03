005bf0b1106659d38e40c778bee1f537a251dce8
mrj9235 <mrj9235@g.rit.edu>
2022-12-02 23:00:01 -0600

    Creating Skeleton for days to make life easier

--- main/src/part1.java
+++ main/src/part1.java
@@ -4,34 +4,6 @@ import java.io.IOException;
 
 public class part1 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader;
-
-        try {
-            reader = new BufferedReader(new FileReader("main/resources/input.txt"));
-            String line = reader.readLine();
-
-            int currSum = 0;
-            int maxSum = 0;
-
-            while(line != null){
-                if(line.equals("")){
-                    if(currSum > maxSum){
-                        maxSum = currSum;
-                    }
-                    currSum = 0;
-                }
-                else{
-                    currSum += Integer.parseInt(line.strip());
-                }
-                line = reader.readLine();
-            }
-
-            System.out.println(maxSum);
-
-        } catch (Exception e){
-            throw e;
-        }
-
 
     }
 }
--- main/src/part2.java
+++ main/src/part2.java
@@ -6,36 +6,6 @@ import java.util.Collections;
 
 public class part2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader;
-
-        try {
-            reader = new BufferedReader(new FileReader("main/resources/input.txt"));
-            String line = reader.readLine();
-
-            int currSum = 0;
-            ArrayList<Integer> allSums = new ArrayList<>();
-
-            while(line != null){
-                if(line.equals("")){
-                    allSums.add(currSum);
-                    currSum = 0;
-                }
-                else{
-                    currSum += Integer.parseInt(line.strip());
-                }
-                line = reader.readLine();
-            }
-
-            Collections.sort(allSums);
-            Collections.reverse(allSums);
-
-            int result = allSums.get(0) + allSums.get(1) + allSums.get(2);
-
-            System.out.println(result);
-
-        } catch (Exception e){
-            throw e;
-        }
 
 
     }
